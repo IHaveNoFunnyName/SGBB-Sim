a1048b6a72af277002b1b3d56edb3408debb2ee1
NoFunnyName <ihavenofunnyname@gmail.com>
2020-10-01 11:06:32 +1000

    added support for macros

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const content = document.getElementById("Content");
-const buttons = ['lp', 'mp', 'hp', 'lk', 'mk', 'hk', "\u2191", "\u2193", "\u2192", "\u2190"];
-let inputs = ["U", "I", "O", "J", "K", "L", "W", "S", "A", "D"];
-let inputCs = ["KeyU", "KeyI", "KeyO", "KeyJ", "KeyK", "KeyL", "KeyW", "KeyS", "KeyA", "KeyD"];
-let inputState = [false, false, false, false, false, false, false, false, false];
+const buttons = ['LP', 'MP', 'HP', 'LK', 'MK', 'HK', "\u2191", "\u2193", "\u2192", "\u2190", "Macro 1", "Macro 2", "RS Assist Up", "RS Assist Down"];
+let inputs = ["U", "I", "O", "J", "K", "L", "W", "S", "A", "D", undefined, undefined, undefined, undefined];
+let inputCs = ["KeyU", "KeyI", "KeyO", "KeyJ", "KeyK", "KeyL", "KeyW", "KeyS", "KeyA", "KeyD", undefined, undefined, undefined, undefined];
+let inputState = [false, false, false, false, false, false, false, false, false, false, false];
 let prevInputState = [false, false, false, false, false, false, false, false, false];
+let macro1 = [true, false, false, false, true, false];
+let macro2 = [false, false, false, false, false, false];
+const macro3 = [false, true, false, true, false, false];
+const macro4 = [false, false, true, false, true, false];
 const imgs = ["Sg_lp", "Sg_mp", "Sg_hp", "Sg_lk", "Sg_mk", "Sg_hk"];
 const punch = [undefined, "C0", "F+", "D+", "F0", "D0", "E0", "C+"];
 const kick = [undefined, "G0", "B0", "G+", "A+", undefined, "A0"];
@@ -11,6 +15,10 @@ const octave = {'1': "+", '0': "0", '-1': "-"};
 let timeout, sound, oldsound;
 let volume = 100;
 let cache = [];
+const mstr1 = "</td><td>"
+const mstr2 = "<td><input type='checkbox' oninput='updateMacros()' class='macrobox'></td>"
+const macroSetStr = "<br><table><tr><td>LP" + mstr1 + "MP" + mstr1 + "HP" + mstr1 + "LK" + mstr1 + "MK" + mstr1 + "HK</td></tr>" + 
+"<tr>" + mstr2 + mstr2 + mstr2 + mstr2 + mstr2 + mstr2 + "</tr></table>"
 
 function displayPlay(){
     document.addEventListener('keydown', playListenerDown);
@@ -26,18 +34,27 @@ function displayPlay(){
 }
 
 function displayOptions(){
-    string = "<button id='bindAll' onClick = 'bindAll()'>Bind All</button><br>  (ESC to cancel)<br>";
+    string = "Volume: <input type=\"range\" min=\"0\" max=\"100\" value=\"" + volume + "\" class=\"slider\" id=\"volume\" oninput='updateVolume()'><div id=\"volumeDisplay\">" + volume + "%</div><br>";
+    string += "<button id='bindAll' onClick = 'bindAll()'>Bind All</button><br>  (ESC to cancel)<br>";
     for ([i, str] of buttons.entries()){
         string += str + ": <button id='" + str + "b' onClick='tryBind(\"" + str + "\")'>";
         string += inputs[i] ? inputs[i].toUpperCase() : "Unbound"; 
-        string += "</button><br>";
+        string += "</button>";
+        string += str.startsWith("Macro") ? macroSetStr : "";
+        string += "</br>";
     }
 
-    string += "Volume: <input type=\"range\" min=\"0\" max=\"100\" value=\"" + volume + "\" class=\"slider\" id=\"volume\" oninput='updateVolume()'><div id=\"volumeDisplay\">100</div><br>";
     string += "Bugs:<br>-Chrome aggressively unloads sounds after a couple minutes idle. A quick workaround minimized this, but it still happens rarely."
     content.innerHTML = string;
 
-    updateVolume();
+    let boxes = document.querySelectorAll('.macrobox');
+    boxes.forEach( (box, i, x) =>{
+        if (i <= 5) {
+            box.checked = macro1[i];
+        } else {
+            box.checked = macro2[i-6];
+        }
+    });
 
     document.removeEventListener('keydown', playListenerDown);
     document.removeEventListener('keyup', playListenerUp);
@@ -49,26 +66,64 @@ function updateVolume() {
     save();
 }
 
+function updateMacros() {
+    let boxes = document.querySelectorAll('.macrobox');
+
+    boxes.forEach( (box, i, x) =>{
+        if (i <= 5) {
+            macro1[i] = box.checked;
+        } else {
+            macro2[i-6] = box.checked;
+        }
+    });
+    save();
+}
+
 async function handleInput() {
     //0 = no change, 1 = direction button pressed or released, 2 = note button released, 3 = note button pressed
     //play notes on 3
     let anything = 0;
 
-    for (i in inputState) {
-        if (inputState[i] != prevInputState[i]){
+    let macroInputState = inputState.slice(0,9);
+
+    if (inputState[10]) {
+        for (i in macro1){
+            macroInputState[i] = macro1[i] ? true : macroInputState[i];
+        }
+    }
+    if (inputState[11]) {
+        for (i in macro2){
+            macroInputState[i] = macro2[i] ? true : macroInputState[i];
+        }
+    }
+    if (inputState[12]) {
+        for (i in macro3){
+            macroInputState[i] = macro3[i] ? true : macroInputState[i];
+        }
+    }
+    if (inputState[13]) {
+        for (i in macro4){
+            macroInputState[i] = macro4[i] ? true : macroInputState[i];
+        }
+    }
+
+    for (i in macroInputState) {
+        if (macroInputState[i] != prevInputState[i]){
             //Give button colour
-            if (i < 6) document.getElementById(imgs[i]).src = "./img/" + imgs[i] + +inputState[i] + ".png";
-            anything = i < 6 ? 2+inputState[i] : 1
+            if (i < 6) {
+                document.getElementById(imgs[i]).src = "./img/" + imgs[i] + +macroInputState[i] + ".png";
+            }
+            anything = i < 6 ? 2+macroInputState[i] : 1
         }
     }
 
     if (anything) {
-        prevInputState = [...inputState];
+        prevInputState = [...macroInputState];
         if (anything === 3) {
-            let note = punch[parseInt("" + +inputState[0] + +inputState[1] + +inputState[2], 2)];
-            if (!note) note = kick[parseInt("" + +inputState[3] + +inputState[4] + +inputState[5], 2)]
+            let note = punch[parseInt("" + +macroInputState[0] + +macroInputState[1] + +macroInputState[2], 2)];
+            if (!note) note = kick[parseInt("" + +macroInputState[3] + +macroInputState[4] + +macroInputState[5], 2)]
             if (note) {
-                oct = inputState[6] - inputState[7];
+                oct = macroInputState[6] - macroInputState[7];
                 oldsound = sound;
                 stop(oldsound);
                 play('snd/' + octave[oct] + note + '.mp3');
@@ -168,7 +223,9 @@ function save(){
     options = {
         inputs: inputs,
         inputCs: inputCs,
-        volume: volume
+        volume: volume,
+        macro1: macro1,
+        macro2: macro2
     }
     localStorage.setItem('options', JSON.stringify(options));
 }
@@ -176,9 +233,11 @@ function save(){
 function load(){
     options = JSON.parse(localStorage.getItem('options'));
     if(options){
-        inputs = options.inputs;
-        inputCs = options.inputCs;
-        volume = options.volume;
+        if (options.inputs) inputs = options.inputs;
+        if (options.inputCs) inputCs = options.inputCs;
+        if (options.volume) volume = options.volume;
+        if (options.macro1) macro1 = options.macro1;
+        if (options.macro2) macro2 = options.macro2;
     }
 }
 
